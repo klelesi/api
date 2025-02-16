15d2e8650fb8997512e89b9d7a7f3e1a35424aa1
Miha Medven <3627889+aweCodeMan@users.noreply.github.com>
2025-02-16 20:57:05 +0100

    force json

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->api(\App\Http\Middleware\ForceJSON::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
